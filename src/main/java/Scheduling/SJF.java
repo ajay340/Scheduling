9f24ee3e4f5869f14aef841f12cf1912235d0206
Ajay Shah <ajay@shah>
2019-11-24 14:03:14 -0500

    added SJF and SRTF java files to extend schedulingabs

--- src/main/java/Scheduling/SJF.java
+++ src/main/java/Scheduling/SJF.java
@@ -1,12 +1,9 @@
 package Scheduling;
-import java.util.Dictionary;
 
-public class SJF{
 
-    public void shortestjob(Process[] processes) {
-        int[][] jobs = new int[processes.length][3];
-        for(int n = 0; n < processes.length; n++){
-            System.out.println(jobs[n]);
-        }
+public class SJF extends SchedulingAbs{
+
+    public SJF(Process[] processes) {
+        super(processes);
     }
 }
---
+++ src/main/java/Scheduling/SRTF.java
@@ -0,0 +1,7 @@
+package Scheduling;
+
+public class SRTF extends SchedulingAbs{
+    public SRTF(Process[] processes){
+        super(processes);
+    }
+}
