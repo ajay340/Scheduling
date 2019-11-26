8e57cee833c9d08330da1da2105bda47ee880d0d
Ajay Shah <ajay@shah>
2019-11-26 00:30:38 -0500

    created override function for gannett chart

--- src/main/java/Scheduling/SRTF.java
+++ src/main/java/Scheduling/SRTF.java
@@ -6,21 +6,19 @@ import java.util.LinkedList;
 import java.util.Comparator;
 
 public class SRTF extends SchedulingAbs{
-    private Queue q = new LinkedList<>();
+    Queue<Process> q = new LinkedList<Process>();
     public SRTF(Process[] processes){
         super(processes);
         System.out.println("SHORTEST REMAINING TIME FIRST (SRTF)");
         System.out.println("------------------------------\n");
 
         Arrays.sort(this.processes, (a, b) -> ((a.arrivalTime == b.arrivalTime) && (a.burstTime < b.burstTime)) ? -1 : ((a.arrivalTime == b.arrivalTime) && (a.burstTime == b.burstTime)) ? 0 : 1); 
-        
         for(int i = 0; i < this.processes.length; i++){
             while(this.processes[i].burstTime != 0){
                 this.processes[i].burstTime = this.processes[i].burstTime - 1;
                 this.processes[i].completionTime = this.processes[i].completionTime + 1;
                 this.processes[i].current_progress += 1;
-                q.add(this.processes[i].ProcessName);
-
+                q.add(this.processes[i]);
                 Arrays.sort(this.processes, new Comparator<Process>() {
                     public int compare(Process u1, Process u2) {
                         u1.current_progress = u2.current_progress;
@@ -37,6 +35,38 @@ public class SRTF extends SchedulingAbs{
                   });
             }               
         }
-        System.out.println(q.toString());
+        // Process[] p = new Process[q.size()];
+        // q.toArray(p);
+        // for(int i = 0; i < p.length; i++){
+        //     for(int j = i; j < p.length; j++){
+        //         if(p[i].ProcessName == p[j].ProcessName){
+        //             p[i].completionTime += 1;
+        //         }
+        //     }
+        // }
+        // for(int i = 0; i < p.length; i++){
+        //     System.out.println(p[i].ProcessName + " " + p[i].completionTime);
+        // }
+    }
+
+    @Override
+    public void printGannttChart(){
+        String ArrivalLine = "        Time  : ";
+        String chart =       "                ";
+        String processLine = "                ";
+        ArrivalLine += "";
+        int aTime = q.peek().arrivalTime;
+        while(q.size() != 0){
+            chart +="+-------";
+            ArrivalLine += aTime + "       ";
+            aTime += 1;
+            processLine += this.q.peek().ProcessName + "      ";
+            q.poll();
+        }
+        chart += "+";
+        ArrivalLine += aTime;
+        System.out.println(ArrivalLine);
+        System.out.println(chart);
+        System.out.println(processLine);
     }
 }
